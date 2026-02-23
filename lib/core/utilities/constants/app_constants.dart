cea97639a73317fc88f04db985c943c3c62dc148
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-02-23 19:12:17 +0200

    feat(home): add TopHeadlines API response models and Dio service implementation

--- lib/core/utilities/constants/app_constants.dart
+++ lib/core/utilities/constants/app_constants.dart
@@ -1,3 +1,10 @@
 class AppConstants {
-  static const appName = 'News App';
+  static const String appName = 'News App';
+  static const String apiKey = 'b0974e624717403f94203ae07ade63f5';
+
+  // APIs
+  static const String baseUrl = 'https://newsapi.org';
+  static const String everything = '/v2/everything';
+  static const String topHeadlines = '/v2/top-headlines';
+  static const String topHeadlinesSources = '/v2/top-headlines/sources';
 }
---
+++ lib/features/Home/services/home_services.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:news_app/core/utilities/constants/app_constants.dart';
+import 'package:news_app/features/Home/models/top_headlines_api_response.dart';
+import 'package:news_app/features/Home/models/top_headlines_body.dart';
+
+class HomeServices {
+  final aDio = Dio();
+
+  Future getTopHeadlines(TopHeadlinesBody body) async {
+    try {
+      aDio.options.baseUrl = AppConstants.baseUrl;
+      final header = {'Authorization': 'Bearer ${AppConstants.apiKey}'};
+      final response = await aDio.get(
+        AppConstants.topHeadlines,
+        queryParameters: body.toMap(),
+        options: Options(headers: header),
+      );
+      if (response.statusCode == 200) {
+        return TopHeadlinesApiResponse.fromMap(response.data);
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
