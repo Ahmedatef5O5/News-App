f6b22032ec310fce025038387b70e963decf6207
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-02-24 01:03:28 +0200

    feat(home) : create custom carousel item widget for home view TopHeadersNews

---
+++ lib/features/Home/widgets/custom_carousel_item.dart
@@ -0,0 +1,114 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/core/utilities/theme/app_colors.dart';
+import 'package:news_app/features/Home/models/top_headlines_api_response.dart';
+
+class CustomCarouselItem extends StatelessWidget {
+  const CustomCarouselItem({super.key, required this.article, this.category});
+  final Article article;
+  final String? category;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.antiAlias,
+
+      decoration: BoxDecoration(
+        color: AppColors.secColor,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12.withValues(alpha: 0.1),
+            blurRadius: 10,
+            spreadRadius: 2,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          CachedNetworkImage(
+            imageUrl: article.urlToImage ?? '',
+            fit: BoxFit.cover,
+            placeholder: (context, url) => const Center(
+              child: CupertinoActivityIndicator(color: Colors.black12),
+            ),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          ),
+
+          Positioned(
+            top: 1,
+            left: 1,
+            child: Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.black12.withAlpha(1),
+
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16),
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  shape: BoxShape.rectangle,
+                  borderRadius: const BorderRadius.all(Radius.circular(18)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 2,
+                  ),
+                  child: Text(
+                    category ?? 'News',
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.black12.withAlpha(1),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Colors.black.withValues(alpha: 0.6),
+                  ],
+                ),
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16),
+                ),
+              ),
+              child: Text(
+                article.title ?? "No title",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
